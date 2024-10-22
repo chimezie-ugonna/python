95dcb11f731f953e23a1345c33d0b0d6506cec78
Ugonna Chimezie <ugiezie@gmail.com>
2024-10-22 10:25:25 +0100

    Added a gender check

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,8 @@
 msg = "Hello World!"
 print(msg)
 name = input("What is your full name?")
-print("Hello " + name)
+gender = input("What is your gender (M/F)?")
+if gender == "M":
+    print("Hello Mr " + name)
+else:
+    print("Hello Ms " + name)
